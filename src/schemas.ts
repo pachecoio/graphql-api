b80297aa2e5da509bdb6f44e5ff9ef34988d7e55
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2021-05-16 19:35:51 -0400

    Fixed docker-compose env variables and packages

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,6 +1,5 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import userSchema from "./users/user.schema";
 import userResolver from "./users/user.resolver";
 
